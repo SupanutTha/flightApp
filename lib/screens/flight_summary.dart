2d5b249b984b83d816ca906fc4e2f3005f5e5e9d
SupanutTha <supanut.thanajirawat@gmail.com>
2023-08-24 11:19:30 +0700

    -add detail in summary page

--- lib/screens/flight_summary.dart
+++ lib/screens/flight_summary.dart
@@ -18,18 +18,66 @@ class FlightSummary extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flight summary'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-              Text('${SelectedFlights.selectedFlights}')
-            // Text('Carrier: ${flight}'),
-            // Text('Flight Number: ${flight}'),
-            // Add more flight details here...
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: SelectedFlights.selectedFlights.length,
+        itemBuilder: (context, index) {
+          Flight SelectedFlight = SelectedFlights.selectedFlights[index];
+          List<dynamic> segments = SelectedFlight.itineraries[0]['segments'];
+          if (segments.length ==1 ){
+            // Only one segment
+            String carrierCode = segments[0]['carrierCode'];
+            String flightNumber = segments[0]['number'];
+            String duration = segments[0]['duration'];
+            String departureIataCode = segments[0]['departure']['iataCode'];
+            String arrivalIataCode = segments[0]['arrival']['iataCode'];
+            String departureTime = segments[0]['departure']['at'];
+            String arrivalTime = segments[0]['arrival']['at'];
+            
+            return ListTile(
+              title: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('$carrierCode$flightNumber: $departureIataCode - $arrivalIataCode'),
+                  Text('Departure Time: $departureTime - Arrival Time: $arrivalTime'),
+                  Text('Duration: $duration'),
+                  Divider()
+                ],
+              ),
+              trailing: Text('\$${SelectedFlight.price['total']}'),
+            );
+
+          } else if (segments.length > 1) {
+                      // Multiple segments
+                      return ListTile(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: segments.map((segment) {
+                            String carrierCode = segment['carrierCode'];
+                            String flightNumber = segment['number'];
+                            String duration = segment['duration'];
+                            String departureIataCode = segment['departure']['iataCode'];
+                            String arrivalIataCode = segment['arrival']['iataCode'];
+                            String departureTime = segment['departure']['at'];
+                            String arrivalTime = segment['arrival']['at'];
+
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('$carrierCode$flightNumber: $departureIataCode - $arrivalIataCode'),
+                              Text('Departure Time: $departureTime - Arrival Time: $arrivalTime'),
+                              Text('Duration: $duration'),
+                            ],
+                          );
+                          }).toList(),
+                        ),
+                        trailing: Text('\$${SelectedFlight.price['total']}'),
+                        
+                      );
+          }
+          
+        },
       ),
+    
     )
     );
   }
